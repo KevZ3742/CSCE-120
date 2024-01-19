c21bd5b18e66077e9ef94d0a7d675b8a5362e32b
KevZ3742 <kzhang3742@gmail.com>
2024-01-19 08:51:43 -0600

    class stuff

---
+++ Class/firstProgram/first.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string name = "";
+    cout << "What is your name?" << endl;
+    cin >> name;
+    cout << name << ", welcome to CSCE 120!";
+}
---
+++ Class/largest/largest.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int largest = 0;
+    cin >> largest;
+    for (unsigned int i = 0; i < 2; ++i)
+    {
+        /* code */
+    }
+}
